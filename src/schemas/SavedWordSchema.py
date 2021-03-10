e0c11801d48f90541ed923360723124d4f4bc9dd
Germs <anthony.kwy@gmail.com>
2021-03-10 17:25:28 +1100

    cleaning up code

--- src/schemas/SavedWordSchema.py
+++ src/schemas/SavedWordSchema.py
@@ -1,18 +1,18 @@
 from main import ma
-from models.SavedWord import SavedWord          # Importing SavedWord Class from Models
+from models.SavedWord import SavedWord
 # from marshmallow.validate import Predicate
-from schemas.UserSchema import UserSchema       # Importing the UserSchema for reference in class below
-from schemas.WordSchema import WordSchema       # Importing the UserSchema for reference in class below
+from schemas.UserSchema import UserSchema
+from schemas.WordSchema import WordSchema
 
 class SavedWordSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
-        model = SavedWord                       # Generating this schema by using the SavedWord model
+        model = SavedWord
         include_fk = True
 
     notification = ma.Boolean(required=True)
-    date_added = ma.String()     # turn to required when basic scaffold done
-    user = ma.Nested(UserSchema)                # nesting my user schema in the savedwordschema
-    word = ma.Nested(WordSchema)                # nesting my word schema in the savedwordschema
+    date_added = ma.String()
+    user = ma.Nested(UserSchema)
+    word = ma.Nested(WordSchema)
 
 
 savedword_schema = SavedWordSchema()
