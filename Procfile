0b5c0680466a9174525d11d5994e92de7bd2e43b
Germs <anthony.kwy@gmail.com>
2021-03-11 16:54:22 +1100

    procfile updated to run create_app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn wsgi --log-file -
+web: gunicorn "main:create_app()"
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
 from src.main import create_app
 if __name__ == "__main__":
-    create_app.run()
+    create_app()
